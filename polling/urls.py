09832a53e5d1341ec8a55cc62e37d2d5052322e4
tylerWVUhagen <tyler.hagen1@yahoo.com>
2022-05-28 19:23:05 -0700

    lesson 8 content complete

--- polling/urls.py
+++ polling/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from polling.views import list_view, detail_view
+from polling.views import PollListView, PollDetailView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>', detail_view, name="poll_detail")  # polling/polls/1
+    path('', PollListView.as_view(), name="poll_index"),
+    # path('', list_view, name="poll_index"),
+    # path('polls/<int:poll_id>', detail_view, name="poll_detail")  # polling/polls/1
+    path('polls/<int:pk>', PollDetailView.as_view(), name="poll_detail")
 ]
